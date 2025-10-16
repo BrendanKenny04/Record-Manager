c13b1a3fc1c5444952740e0ca6f98940a51687d2
Brendan Kenny <sirgreendog@gmail.com>
2025-10-15 19:30:53 -0500

    generic push

--- assign3/record_mgr.c
+++ assign3/record_mgr.c
@@ -4,32 +4,45 @@
 #include "record_mgr.h"
 
 // table and manager
-RC initRecordManager (void *mgmtData);
-RC shutdownRecordManager ();
-RC createTable (char *name, Schema *schema);
-RC openTable (RM_TableData *rel, char *name);
-RC closeTable (RM_TableData *rel);
-RC deleteTable (char *name);
-int getNumTuples (RM_TableData *rel);
+RC initRecordManager (void *mgmtData){}
+RC shutdownRecordManager (){}
+RC createTable (char *name, Schema *schema){
+    //Initialize the table's data structure
+    RM_TableData *newTable = malloc(sizeof(RM_TableData));
+    newTable->name=name;
+    newTable->schema=schema;
+
+    //
+
+    //Finally, create the page file that will store the table's data, and put in the table's metadata
+    createPageFile(name);
+    SM_FileHandle* tableFile = malloc(sizeof(SM_FileHandle));
+    openPageFile(name,tableFile);
+    
+}
+RC openTable (RM_TableData *rel, char *name){}
+RC closeTable (RM_TableData *rel){}
+RC deleteTable (char *name){}
+int getNumTuples (RM_TableData *rel){}
 
 // handling records in a table
-RC insertRecord (RM_TableData *rel, Record *record);
-RC deleteRecord (RM_TableData *rel, RID id);
-RC updateRecord (RM_TableData *rel, Record *record);
-RC getRecord (RM_TableData *rel, RID id, Record *record);
+RC insertRecord (RM_TableData *rel, Record *record){}
+RC deleteRecord (RM_TableData *rel, RID id){}
+RC updateRecord (RM_TableData *rel, Record *record){}
+RC getRecord (RM_TableData *rel, RID id, Record *record){}
 
 // scans
-RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond);
-RC next (RM_ScanHandle *scan, Record *record);
-RC closeScan (RM_ScanHandle *scan);
+RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond){}
+RC next (RM_ScanHandle *scan, Record *record){}
+RC closeScan (RM_ScanHandle *scan){}
 
 // dealing with schemas
-int getRecordSize (Schema *schema);
-Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *typeLength, int keySize, int *keys);
-RC freeSchema (Schema *schema);
+int getRecordSize (Schema *schema){}
+Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *typeLength, int keySize, int *keys){}
+RC freeSchema (Schema *schema){}
 
 // dealing with records and attribute values
-RC createRecord (Record **record, Schema *schema);
-RC freeRecord (Record *record);
-RC getAttr (Record *record, Schema *schema, int attrNum, Value **value);
-RC setAttr (Record *record, Schema *schema, int attrNum, Value *value);
+RC createRecord (Record **record, Schema *schema){}
+RC freeRecord (Record *record){}
+RC getAttr (Record *record, Schema *schema, int attrNum, Value **value){}
+RC setAttr (Record *record, Schema *schema, int attrNum, Value *value){}
